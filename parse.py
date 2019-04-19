edbbb21c70d28ea37ef4b5666813e923575f3630
ACanadianMoose <olivierjuteaud@gmail.com>
2019-04-19 16:31:35 -0400

    added fonctions

--- parse.py
+++ parse.py
@@ -1,5 +1,24 @@
 import csv
 
+def find(file, key, returnRow):
+    
 
+
+def findRouteID(lineId):
+    return find(fichier,)
+
+def findStops(lineNb):
+    routeId = findRouteID(lineId)
+
+    tripId = findTripID(routeId)
+
+    stopId = findStopId(tripId)
+
+    return findStopName(stopId)
+    
+
+def findLine( search):
+
+    findLineWithNumber(search)
 
 
